c25ac43f969ce0b96641addb377d8b12fb3cdd52
Damian Lisas <damian.lisas@gmail.com>
2021-05-02 17:24:04 -0300

    CU-4h3czw - Proyecto base

---
+++ src/test/kotlin/com/compumundohipermegaweb/hefesto/api/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.compumundohipermegaweb.hefesto.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
