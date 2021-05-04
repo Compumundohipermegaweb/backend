04bd9b773c1db9d4fcba2f4947743a97d161de36
Damian Lisas <damian.lisas@gmail.com>
2021-05-03 21:47:09 -0300

    Se permite cualquier origen en el Ping

--- src/main/kotlin/com/compumundohipermegaweb/hefesto/api/ping/rest/PingController.kt
+++ src/main/kotlin/com/compumundohipermegaweb/hefesto/api/ping/rest/PingController.kt
@@ -2,10 +2,12 @@ package com.compumundohipermegaweb.hefesto.api.ping.rest
 
 import com.compumundohipermegaweb.hefesto.api.ping.domain.action.Ping
 import com.compumundohipermegaweb.hefesto.api.ping.domain.model.PingResponse
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/ping")
 class PingController(private val ping: Ping) {
