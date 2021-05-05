fe6d427d8a062e02301c62df446208a2dd4b4cdc
Costalesjonatan <costalesjonatan@gmail.com>
2021-05-04 23:40:58 -0300

    Se agregaron anotaciones a las entidades rest - CU-4h3dtm

--- src/main/kotlin/com/compumundohipermegaweb/hefesto/api/client/rest/ClientRequest.kt
+++ src/main/kotlin/com/compumundohipermegaweb/hefesto/api/client/rest/ClientRequest.kt
@@ -1,9 +1,13 @@
 package com.compumundohipermegaweb.hefesto.api.client.rest
 
-data class ClientRequest(val documentNumber: String,
-                         val firstName: String,
-                         val lastName: String,
-                         val surName: String,
-                         var category: String,
-                         var email: String,
-                         var contactNumber: String)
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonProperty
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class ClientRequest(@JsonProperty val documentNumber: String,
+                         @JsonProperty val firstName: String,
+                         @JsonProperty val lastName: String,
+                         @JsonProperty val surName: String,
+                         @JsonProperty var category: String,
+                         @JsonProperty var email: String,
+                         @JsonProperty var contactNumber: String)
--- src/main/kotlin/com/compumundohipermegaweb/hefesto/api/client/rest/ClientResponse.kt
+++ src/main/kotlin/com/compumundohipermegaweb/hefesto/api/client/rest/ClientResponse.kt
@@ -1,7 +1,9 @@
 package com.compumundohipermegaweb.hefesto.api.client.rest
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class ClientResponse(@JsonProperty("document_number") val documentNumber: String,
                           @JsonProperty("fist_name")val firstName: String,
                           @JsonProperty("last_name")val lastName: String,
