afd09e8b222dd1645c083e5d30f6f8c95d9d5579
Damian Lisas <damian.lisas@gmail.com>
2021-05-13 00:28:55 -0300

    Agregada annotation faltante al request body. CU-4h3dtm

--- src/main/kotlin/com/compumundohipermegaweb/hefesto/api/client/rest/controller/ClientController.kt
+++ src/main/kotlin/com/compumundohipermegaweb/hefesto/api/client/rest/controller/ClientController.kt
@@ -9,6 +9,7 @@ import com.compumundohipermegaweb.hefesto.api.client.rest.representation.ClientR
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController
 class ClientController(private val registerClient: RegisterClient, private val findClients: FindClients) {
 
     @PostMapping
-    fun postClient(clientRequest: ClientRequest) : ResponseEntity<ClientResponse> {
+    fun postClient(@RequestBody clientRequest: ClientRequest) : ResponseEntity<ClientResponse> {
         return ResponseEntity.ok(registerClient(clientRequest).toClientResponse())
     }
 
