ed4d57e38c3f0956969ac98a47eebf6177290f0d
Damian Lisas <damian.lisas@gmail.com>
2021-05-17 23:57:36 -0300

    Eliminada respuesta mock del branch controller. CU-4naqb6

--- src/main/kotlin/com/compumundohipermegaweb/hefesto/api/branch/rest/controller/BranchController.kt
+++ src/main/kotlin/com/compumundohipermegaweb/hefesto/api/branch/rest/controller/BranchController.kt
@@ -35,14 +35,7 @@ class BranchController (private val registerBranch: RegisterBranch,
         val items = findStockedItems(searchCriteria)
         val itemResponse = items.map { it.toItemStockResponse() }
 
-        //return ResponseEntity.ok(StockResponse(itemResponse))
-        return ResponseEntity.ok(StockResponse(listOf(
-                ItemStockResponse(1,"1", "Martillo", "Martillo de carpintero", "Redline", 1250.0, 20, true),
-                ItemStockResponse(2,"2", "Martillo", "Martillo de mango largo", "", 1322.99, 34, true),
-                ItemStockResponse(3,"3", "Martillo", "Martillo de mango corto", "", 1450.0, 21, true),
-                ItemStockResponse(4,"4", "Martillo", "Martillo nashe", "", 1200.99, 30, true),
-                ItemStockResponse(5,"5", "Martillo", "Martillito re loco", "", 989.99, 6, true)
-        )))
+        return ResponseEntity.ok(StockResponse(itemResponse))
     }
 
     private fun ItemStock.toItemStockResponse(): ItemStockResponse {
