5508735e993cf3a37e87bf8f81138600dc27b301
Costalesjonatan <costalesjonatan@gmail.com>
2021-05-20 19:15:37 -0300

    Se agrego la logica de generación de numero de factura. - CU-4v7ggv

--- src/main/kotlin/com/compumundohipermegaweb/hefesto/api/invoice/domain/model/Invoice.kt
+++ src/main/kotlin/com/compumundohipermegaweb/hefesto/api/invoice/domain/model/Invoice.kt
@@ -5,7 +5,7 @@ import com.compumundohipermegaweb.hefesto.api.sale.domain.model.SaleDetails
 import java.util.*
 
 data class Invoice(val id: Long,
-                   val voucherNumber: String,
+                   var voucherNumber: String,
                    val billingDate: Date,
                    val type: String,
                    val client: Client,
--- src/main/kotlin/com/compumundohipermegaweb/hefesto/api/invoice/infra/repository/JpaInvoiceRepository.kt
+++ src/main/kotlin/com/compumundohipermegaweb/hefesto/api/invoice/infra/repository/JpaInvoiceRepository.kt
@@ -6,16 +6,30 @@ import com.compumundohipermegaweb.hefesto.api.invoice.infra.representation.Invoi
 
 class JpaInvoiceRepository(private val springDataInvoiceClient: SpringDataInvoiceClient): InvoiceRepository  {
     override fun save(invoice: Invoice): Invoice {
-        val savedInvoice = springDataInvoiceClient.save(invoice.toInvoiceDao())
-        savedInvoice.voucherNumber = "0000000" + savedInvoice.id
-        return springDataInvoiceClient.save(savedInvoice).toInvoice(invoice)
+        val invoiceSaved = springDataInvoiceClient.save(invoice.toInvoiceDao())
+        return springDataInvoiceClient.save(buildVoucherNumber(invoiceSaved)).toInvoice(invoice, invoiceSaved.voucherNumber)
     }
 
-    private fun InvoiceDao.toInvoice(invoice: Invoice): Invoice {
-        return Invoice(id, documentType+"0000"+branchId+voucherNumber, billingDate, type, invoice.client, branchId, invoice.branchAddress, invoice.branchContact, invoice.cuit, invoice.activitySince, invoice.saleDetails, subTotal, ivaSubTotal, total)
+    private fun buildVoucherNumber(invoiceDao: InvoiceDao): InvoiceDao {
+        val voucherNumber = invoiceDao.id.toString()
+        val quantityOfZeros = StringBuilder()
+        for(i in voucherNumber.length..7){ quantityOfZeros.append("0") }
+        invoiceDao.voucherNumber = quantityOfZeros.toString()+voucherNumber
+        return invoiceDao
+    }
+
+    private fun buildBranchNumber(branchId: Long): String {
+        val branchNumber = branchId.toString()
+        val quantityOfZeros = StringBuilder()
+        for(i in branchNumber.length..4){ quantityOfZeros.append("0") }
+        return quantityOfZeros.toString()+branchNumber
+    }
+
+    private fun InvoiceDao.toInvoice(invoice: Invoice, voucherNumber: String): Invoice {
+        return Invoice(id, documentType+type+buildBranchNumber(branchId)+voucherNumber, billingDate, type, invoice.client, branchId, invoice.branchAddress, invoice.branchContact, invoice.cuit, invoice.activitySince, invoice.saleDetails, subTotal, ivaSubTotal, total)
     }
 
     private fun Invoice.toInvoiceDao(): InvoiceDao {
-        return InvoiceDao(id, voucherNumber, billingDate, "FV$type", type, branchId, subTotal, ivaSubTotal, total)
+        return InvoiceDao(id, voucherNumber, billingDate, "FV", type, branchId, subTotal, ivaSubTotal, total)
     }
 }
