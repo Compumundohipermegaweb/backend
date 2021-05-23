61bb84e8ecf00c3fac856386ad2bc6a9143ed2bf
Damian Lisas <damian.lisas@gmail.com>
2021-05-22 23:06:49 -0300

    Agregadas annotation necesarias a las query update para que funcionen. CU-4q4ycd

--- src/main/kotlin/com/compumundohipermegaweb/hefesto/api/checking/account/infra/repository/SpringDataCheckingAccountDao.kt
+++ src/main/kotlin/com/compumundohipermegaweb/hefesto/api/checking/account/infra/repository/SpringDataCheckingAccountDao.kt
@@ -1,15 +1,21 @@
 package com.compumundohipermegaweb.hefesto.api.checking.account.infra.repository
 
 import com.compumundohipermegaweb.hefesto.api.checking.account.infra.representation.CheckingAccountRepresentation
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.CrudRepository
+import javax.transaction.Transactional
 
 interface SpringDataCheckingAccountDao: CrudRepository<CheckingAccountRepresentation, Long> {
     fun findByClientId(clientId: Long): CheckingAccountRepresentation?
 
+    @Transactional
+    @Modifying
     @Query("update CheckingAccountRepresentation c set c.balanceDue = :amount where c.clientId = :clientId")
     fun updateBalanceDueByClient(clientId: Long, amount: Double)
 
+    @Transactional
+    @Modifying
     @Query("update CheckingAccountRepresentation c set c.balance = :amount where c.clientId = :clientId")
     fun updateBalanceByClient(clientId: Long, amount: Double)
 }
